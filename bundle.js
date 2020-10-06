bc17bc2c9d81ac71208814ae6b01833576d5e1ce
diana <devdianaregina@gmail.com>
2020-10-05 22:11:53 -0300

    first commit

---
+++ bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var arr = [1, 3, 4, 3];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
+
+var teste = function teste() {
+  return 'teste';
+};
+
+console.log(teste());
---
+++ main.js
@@ -0,0 +1,11 @@
+const arr = [1, 3 , 4, 3, ];
+
+const newArr = arr.map(item => item * 2
+ );
+
+console.log(newArr);
+
+const teste = () => {
+    return 'teste';
+}
+console.log(teste());
