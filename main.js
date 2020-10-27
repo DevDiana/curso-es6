a4768140cdb8e86088aea4378c49a2c4c6e0eb52
DevDiana <devdianaregina@gmail.com>
2020-10-27 13:50:51 -0300

    enviando desestruturaçao

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const arr = [1, 3 , 4, 3, ];
 
 const newArr = arr.map(item => item * 2
@@ -9,3 +10,21 @@ const teste = () => {
     return 'teste';
 }
 console.log(teste());
+*/
+
+const usuario = {
+    nome: 'Diana',
+    idade: 23,
+    endereco:{
+        cidade: 'Rio do Sul',
+        estado: 'SC',
+
+    },
+};
+
+function mostraNome({nome, idade}){
+    console.log(nome, idade);
+
+}
+
+mostraNome(usuario);
